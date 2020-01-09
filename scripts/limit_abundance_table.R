67bbf49523da462fdf6748fecfba610cf3eda333
mkamenet3 <maria.kamenetsky@gmail.com>
2020-01-09 16:59:39 -0600

    r code to create excel-able samples and added dataset1. Next steps: need to create new dataset doc with new rows for frustration part of reproducible example

--- scripts/limit_abundance_table.R
+++ scripts/limit_abundance_table.R
@@ -3,3 +3,20 @@
 #M.Kamenetsky
 #2020-01-08
 #################################################
+library(tidyverse)
+
+full <- read.delim("data/abundance_table.txt", header=TRUE)
+reduced <- full %>%
+    dplyr::select(taxonomy, X.OTU.ID, L1S8,L1S140,L5S155, L6S20,L2S204,L2S175) %>%
+    slice(1:100)
+reduced_long <- reduced %>%
+    tidyr::gather(sampleid, otus, L1S8:L2S175, factor_key=TRUE) %>%
+    mutate(bodysite = if_else(sampleid == "L1S8"| sampleid=="L1S140", "gut",
+                              if_else(sampleid=="L5S155" | sampleid=="L6S20", "tongue",
+                                      if_else(sampleid=="L2S204" | sampleid=="L2S175", "leftpalm", "NA")))) %>%
+    mutate(bodysite = as.factor(bodysite)) 
+clean1 <- reduced_long %>%
+    dplyr::filter(otus!=0) %>%
+    dplyr::select(sampleid, otus, bodysite)
+
+
