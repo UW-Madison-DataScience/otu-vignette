84607c79642d786c08b1d37ac66e84d6d35b38de
Sailendharan <42844162+Sailendharan@users.noreply.github.com>
2020-01-21 16:21:19 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,10 @@
 install.packages(c("ggplot2",
                    "dplyr",
-                   "tidyr"))
+                   "tidyr", 
+                   "readr", 
+                   "OTUtable"))
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+install.packages("BiocManager")
+BiocManager::install("microbiome")
+BiocManager::install("phyloseq")
